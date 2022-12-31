cdf5001f795a49c5b6cf1c0b7767ab790139e486
gruebel <anton.gruebel@gmail.com>
2023-01-01 00:42:13 +0100

    fix linting

--- extra_stubs/flask_apscheduler/scheduler.pyi
+++ extra_stubs/flask_apscheduler/scheduler.pyi
@@ -7,16 +7,11 @@ _F = TypeVar("_F", bound=Callable[..., Any])
 _T = TypeVar("_T")
 _P = ParamSpec("_P")
 
-
 class APScheduler:
     def __init__(self, scheduler: BaseScheduler | None = ..., app: Flask | None = ...) -> None: ...
-
     @overload
     def task(self, func: Callable[_P, _T]) -> Callable[_P, _T]: ...
-
     @overload
     def task(self, trigger: str, *, id: str, minute: str | None = None) -> Callable[[_F], _F]: ...
-
     def init_app(self, app: Flask) -> None: ...
-
     def start(self, paused: bool = ...) -> None: ...
